a1de4936badbcc826b70149ab61db332fd521762
8minus1Trout <u1040168@gmail.com>
2024-11-20 20:51:57 +0800

    add login, register feature

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from models import db
+from flask_login import LoginManager, login_required, current_user
+from models import db, User, Post
 from config import Config
+from auth import auth_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+login_manager.login_message = '請先登入以訪問此頁面'
+login_manager.login_message_category = 'warning'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# Register Blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
 @app.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
 
 if __name__ == '__main__':
     app.run(debug=True)
