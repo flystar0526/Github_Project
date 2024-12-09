e488b9d90e9046332b326be9cc85326c4a27f702
flystar0526 <flowermetoer@gmail.com>
2024-12-10 00:40:05 +0800

    Use App Engine

--- utils/gcs.py
+++ utils/gcs.py
@@ -3,20 +3,16 @@ import os
 from uuid import uuid4
 
 class GCSHelper:
-  def __init__(self, bucket_name, key_file):
+  def __init__(self, bucket_name):
     self.bucket_name = bucket_name
-    self.client = storage.Client.from_service_account_json(key_file)
+    self.client = storage.Client()
     self.bucket = self.client.bucket(bucket_name)
 
   def upload_file(self, file, folder):
     filename = f"{folder}/{uuid4().hex}{os.path.splitext(file.filename)[1]}"
     blob = self.bucket.blob(filename)
     blob.upload_from_file(file, content_type=file.content_type)
-    # 不需要設置 ACL，使用 Bucket 的 IAM 控制
     return f"https://storage.googleapis.com/{self.bucket_name}/{filename}"
 
-# 初始化 GCS 工具
-gcs_helper = GCSHelper(
-    bucket_name=os.getenv('GCS_BUCKET_NAME'),
-    key_file=os.getenv('GCS_KEY_FILE')
-)
+# Initialize GCS helper
+gcs_helper = GCSHelper(bucket_name=os.getenv('GCS_BUCKET_NAME'))
